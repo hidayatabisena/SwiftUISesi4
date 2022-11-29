0be75b073a0f510f4e17d340ccf4bf1ae7af59cb
Hidayat Abisena <plegmasena@gmail.com>
2022-11-29 22:22:06 +0700

    Initial commit

---
+++ SwiftUISesi4/SwiftUISesi4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISesi4App.swift
+//  SwiftUISesi4
+//
+//  Created by Hidayat Abisena on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISesi4App: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
